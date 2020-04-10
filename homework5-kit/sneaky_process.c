786177590ee69a60ab34a1ff319a531836562065
bh214 <bh214@duke.edu>
2020-04-10 18:24:19 -0400

    adding read-from-console function for process

--- homework5-kit/sneaky_process.c
+++ homework5-kit/sneaky_process.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
+#include <string.h> // for strcmp()
 
 int main() {
   printf("sneaky_process pid = %d\n", getpid());
-  system("ls");
+  system("sudo insmod sneaky_mod.ko");
+
+  char* line = NULL;
+  size_t size;
+  int ctn = 1;
+  while (ctn) {
+    getline(&line, &size, stdin);
+    if (strcmp(line, "q") == 0) {
+      ctn = 0;
+    } else {
+      system(line);
+    }
+  }
+  
+  system("sudo rmmod sneaky_mod");
+  
   return EXIT_SUCCESS;
 }
