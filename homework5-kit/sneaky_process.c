8fa437a22658ed21a7a1087ed7efc1f9bde8ae94
bh214 <bh214@duke.edu>
2020-04-10 18:47:55 -0400

    s_process terminal continuous execution success

--- homework5-kit/sneaky_process.c
+++ homework5-kit/sneaky_process.c
@@ -3,24 +3,27 @@
 #include <unistd.h>
 #include <string.h> // for strcmp()
 
-int main() {
-  printf("sneaky_process pid = %d\n", getpid());
-  system("sudo insmod sneaky_mod.ko");
-
+void execv_line() {
   char* line = NULL;
   size_t size;
   int ctn = 1;
   while (ctn) {
     getline(&line, &size, stdin);
-    printf("%s", line);
-    if (strcmp(line, "q") == 0) {
+    if (strcmp(line, "q\n") == 0) {
       ctn = 0;
     } else {
       system(line);
     }
   }
+}
+
+int main() {
+  printf("sneaky_process pid = %d\n", getpid());  // print pid
+  system("sudo insmod sneaky_mod.ko");  // load mod
+
+  execv_line(); // execute terminal commands
   
-  system("sudo rmmod sneaky_mod");
+  system("sudo rmmod sneaky_mod");     // un-load mod
   
   return EXIT_SUCCESS;
 }
