ad9360aa50e7fc9168982ebbd3d85279481eb8d4
bh214 <bh214@duke.edu>
2020-05-10 22:08:40 -0400

    remove unnecessary files

--- homework5-kit/.sneaky_mod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bh214/650-prj5/homework5-kit/sneaky_mod.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/bh214/650-prj5/homework5-kit/sneaky_mod.ko /home/bh214/650-prj5/homework5-kit/sneaky_mod.o /home/bh214/650-prj5/homework5-kit/sneaky_mod.mod.o
